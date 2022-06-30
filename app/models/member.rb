5d97f538fb5e6bb3e9d5b6354d2ea9603d8a42e4
Taishi A <arataitai@gmail.com>
2022-06-30 12:14:12 +0000

    validation setting

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,7 @@
 class Member < ApplicationRecord
   has_many :carts
-  validates :email, { uniqueness: true }
+  validates :first_name, { presence: true }
+  validates :last_name, { presence: true }
+  validates :email, { uniqueness: true, presence: true }
+  validates :password, { presence: true }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :carts
+  validates :name, { presence: true }
+  validates :price, { presence: true }
+  validates :alcohol, { presence: true }
+  validates :image_path, { presence: true }
+  validates :material, { presence: true }
+  validates :product_area, { presence: true }
 end
