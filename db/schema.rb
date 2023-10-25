ce52b8ddaabfca72f3c0fc4e69e52d7deae790b3
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-10-25 23:23:33 +0900

    Memberモデルのpasswordカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_25_093204) do
+ActiveRecord::Schema.define(version: 2023_10_25_141608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2023_10_25_093204) do
     t.string "last_name"
     t.string "first_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
