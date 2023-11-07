bb115851469b3f13f894625ab57a5014a85bb5f5
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-11-07 10:50:30 +0900

    ユーザー新規登録テストを修正

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -2,48 +2,60 @@ require 'rails_helper'
 
 RSpec.describe "Members", type: :request do
   describe "GET /members" do
-    it "ステータス200が返ってくる" do
+    it "新規登録画面に遷移する" do
       get new_member_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST /members" do
-    let(:member_params) do
-      { member: {
-        last_name: "nomu",
-        first_name: "sho",
-        email: "email@example.com",
-        password: "password"
-        }
-      }
-    end
-    context "バリデーションが有効な場合" do
-      it "新規登録ができる" do
+    describe "ユーザー新規登録"  do
+      let(:last_name) { "hoge" }
+      let(:first_name) { "fuga" }
+      let(:email) { "email@example.com" }
+      let(:password) { "password" }
+      let(:member_params) { { member: { last_name: last_name, first_name: first_name, email: email, password: password} } }
+
+      before do
         post members_path, params: member_params
-        expect(response).to have_http_status(:found)
-      end
-    end
-
-    context "バリデーションが無効な場合" do
-      it "姓が無い場合" do
-        post members_path, params: member_params.merge(member: { last_name: nil })
-        expect(response).to have_http_status(:unprocessable_entity)
       end
 
-      it "名が無い場合" do
-        post members_path, params: member_params.merge(member: { first_name: nil })
-        expect(response).to have_http_status(:unprocessable_entity)
+      context "last_name,first_name,email,passwordを入力した場合" do
+        it "新規登録ができる" do
+          expect(response).to have_http_status(:found)
+        end
       end
 
-      it "emailが無い場合" do
-        post members_path, params: member_params.merge(member: { email: nil })
-        expect(response).to have_http_status(:unprocessable_entity)
+      context "last_nameが無い場合" do
+        let(:last_name) { nil }
+
+        it "新規登録ができない" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
       end
 
-      it "passwordが無い場合" do
-        post members_path, params: member_params.merge(member: { password: nil })
-        expect(response).to have_http_status(:unprocessable_entity)
+      context "first_nameが無い場合" do
+        let(:first_name) { nil }
+
+        it "新規登録ができない" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "emailが無い場合" do
+        let(:email) { nil }
+
+        it "新規登録ができない" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "passwordが無い場合" do
+        let(:password) { nil }
+
+        it "新規登録ができない" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
       end
     end
   end
