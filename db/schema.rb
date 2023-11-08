20d978f3e4e27f4e2c8e548b3990de77e7f3cec8
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-11-08 16:08:10 +0900

    deviseとmerge後にslimに変換

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_07_01_232451) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_141608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,11 +32,11 @@ ActiveRecord::Schema[7.1].define(version: 2022_07_01_232451) do
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
