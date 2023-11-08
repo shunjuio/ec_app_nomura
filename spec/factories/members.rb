31715c4a0df3039eac919f88ef3b148e2acf8654
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-11-08 12:14:01 +0900

    商品をカートに入れるテストを作成

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :member do
+    id { 1 }
     last_name { "nomu" }
     first_name { "sho" }
     email { "email@example.com" }
---
+++ spec/system/products_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Products", type: :system do
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "商品詳細ページ" do
+    let(:product) { create(:product) }
+
+    it "商品詳細ページが表示される" do
+      visit product_path(product)
+      expect(page).to have_button "カートに入れる"
+    end
+  end
+
+  describe "カートに商品を入れる" do
+    let(:product) { create(:product) }
+    let(:member) { create(:member) }
+    let(:email) { member.email }
+    let(:password) { member.password }
+    let(:cart) { build(:cart) }
+
+    before do
+      visit members_login_path
+      fill_in "email", with: email
+      fill_in "password", with: password
+      find('input[name="commit"]').click
+    end
+
+    it "カートに商品が入る" do
+      visit product_path(product)
+      expect(page).to have_selector("#product_id", visible: false)
+      expect(page).to have_selector("#member_id", visible: false)
+
+      fill_in "quantity", with: cart.quantity
+      expect{ find('input[name="commit"]').click }.to change { Cart.count }.by(1)
+      expect(current_path).to eq cart_index_path
+      expect(page).to have_button "購入手続きへ進む"
+    end
+  end
+end
