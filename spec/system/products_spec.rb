7fc0ea316a831be339c8eb335ef4a115a6165ea9
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-11-09 11:52:37 +0900

    商品をクリックして画面遷移するように修正

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,32 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :system do
-  let(:product) { create(:product) }
-
   before do
     driven_by(:rack_test)
   end
 
   describe "商品詳細ページ" do
-    it "商品詳細ページが表示される" do
-      visit product_path(product)
-      expect(page).to have_button "カートに入れる"
-    end
-  end
-
-  describe "カートに商品を入れる" do
+    let!(:product) { create(:product) }
     let(:member) { create(:member) }
     let(:cart) { build(:cart) }
 
     before do
       sign_in member
+      visit root_path
+    end
+
+    it "商品詳細ページが表示される" do
+      click_link "響"
+      expect(current_path).to eq product_path(product)
+      expect(page).to have_button "カートに入れる"
     end
 
     it "カートに商品が入る" do
       visit product_path(product)
-      expect(page).to have_selector("#product_id", visible: false)
-      expect(page).to have_selector("#member_id", visible: false)
-
       fill_in "quantity", with: cart.quantity
       expect{ find('input[name="commit"]').click }.to change { Cart.count }.by(1)
       expect(current_path).to eq cart_index_path
