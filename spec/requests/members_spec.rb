5d402890e71b77d040d7e68909378b45495b0742
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-12-20 18:07:26 +0900

    ネストの深さを修正

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -1,61 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe 'Members' do
-  describe 'GET /members' do
-    it '新規登録画面に遷移する' do
+  describe '新規登録画面に遷移' do
+    it '新規登録画面に遷移できる' do
       get new_member_registration_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'POST /members' do
-    describe 'ユーザー新規登録' do
-      let(:last_name) { 'hoge' }
-      let(:first_name) { 'fuga' }
-      let(:email) { 'email@example.com' }
-      let(:password) { 'password' }
-      let(:member_params) { { member: { last_name:, first_name:, email:, password: } } }
+  describe 'ユーザー新規登録' do
+    let(:last_name) { 'hoge' }
+    let(:first_name) { 'fuga' }
+    let(:email) { 'email@example.com' }
+    let(:password) { 'password' }
+    let(:member_params) { { member: { last_name:, first_name:, email:, password: } } }
 
-      before do
-        post member_registration_path, params: member_params
+    before do
+      post member_registration_path, params: member_params
+    end
+
+    context 'last_name,first_name,email,passwordを入力した場合' do
+      it '新規登録ができる' do
+        expect(response).to have_http_status(:see_other)
       end
+    end
 
-      context 'last_name,first_name,email,passwordを入力した場合' do
-        it '新規登録ができる' do
-          expect(response).to have_http_status(:see_other)
-        end
+    context 'last_nameが無い場合' do
+      let(:last_name) { nil }
+
+      it '新規登録ができない' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
+    end
 
-      context 'last_nameが無い場合' do
-        let(:last_name) { nil }
+    context 'first_nameが無い場合' do
+      let(:first_name) { nil }
 
-        it '新規登録ができない' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+      it '新規登録ができない' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
+    end
 
-      context 'first_nameが無い場合' do
-        let(:first_name) { nil }
+    context 'emailが無い場合' do
+      let(:email) { nil }
 
-        it '新規登録ができない' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+      it '新規登録ができない' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
+    end
 
-      context 'emailが無い場合' do
-        let(:email) { nil }
+    context 'passwordが無い場合' do
+      let(:password) { nil }
 
-        it '新規登録ができない' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-      end
-
-      context 'passwordが無い場合' do
-        let(:password) { nil }
-
-        it '新規登録ができない' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+      it '新規登録ができない' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
