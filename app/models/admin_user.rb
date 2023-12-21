f454d00d579b38280343a6bba2c6bc1298567046
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-12-21 11:54:07 +0900

    2行を1行に修正

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[id id_value email encrypted_password reset_password_token reset_password_sent_at
