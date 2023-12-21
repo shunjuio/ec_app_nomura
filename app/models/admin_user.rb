30c6e49be0a3620c369b0322a99aa73d02e17178
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-12-21 11:30:01 +0900

    式展開の記述を修正

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   def self.ransackable_attributes(_auth_object = nil)
