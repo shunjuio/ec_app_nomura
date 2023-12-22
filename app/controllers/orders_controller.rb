08959eb30e68eb93220ee207265d7e9e3a28ece6
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-12-22 18:23:36 +0900

    orders_controller/newから、合計金額を算出するロジックを分離
    orders_controller/createから、OrderProductモデルに保存する機能を分離
    
    order/editアクションを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,47 +10,29 @@ class OrdersController < ApplicationController
 
   # GET /orders/1 or /orders/1.json
   def show
-    @member = Member.find_by(id: current_member.id)
     @order_products = OrderProduct.where(order_id: @order.id).includes(:product)
-    @index = 0
   end
 
   # GET /orders/new
   def new
-    @member = Member.find_by(id: current_member.id)
-    @carts = Cart.where(member_id: @member[:id]).includes(:product)
-    @index = 0
-    total_prices = @carts.map do |cart|
-      cart.product.price * cart.quantity
-    end
-    @total_price = total_prices.sum
-    @total_quantity = @carts.sum(:quantity)
-    @postage = 500
-    @amount_billed = @total_price + @postage
+    @carts = Cart.where(member_id: current_member.id).includes(:product)
+    calculate_totals
     @order = Order.new
   end
 
   # GET /orders/1/edit
-  def edit
-    @member = Member.find_by(id: current_member.id)
-  end
+  def edit;  end
 
   # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
-    @member = Member.find_by(id: current_member.id)
-    @carts = Cart.where(member_id: @member[:id]).includes(:product)
+    @carts = Cart.where(member_id: current_member.id).includes(:product)
 
     respond_to do |format|
       if @order.save
         format.html { redirect_to order_url(@order), notice: t('orders.create') }
         format.json { render :show, status: :created, location: @order }
-        # order_productへ商品と個数を登録して、cartから商品を削除する処理を書く
-        @carts.each do |cart|
-          @order_product = OrderProduct.new(product_id: cart.product_id, order_id: @order.id, quantity: cart.quantity)
-          @order_product.save
-          cart.destroy
-        end
+        save_order_product
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
@@ -92,5 +74,25 @@ class OrdersController < ApplicationController
   def order_params
     params[:order].permit(:member_id, :postage, :amount_billed, :payment_method, :shipping_address, :postal_code,
                           :purchaser_last_name, :purchaser_first_name, :purchaser_email)
+  end
+
+  def calculate_totals
+    total_prices = @carts.map { |cart| cart.product.price * cart.quantity }
+    @total_price = total_prices.sum
+    @total_quantity = @carts.sum(:quantity)
+    @postage = 500
+    @amount_billed = @total_price + @postage
+  end
+
+  def save_order_product
+    @carts.each do |cart|
+      @order_product = OrderProduct.new(product_id: cart.product_id, order_id: @order.id, quantity: cart.quantity)
+      @order_product.save
+      delete_cart(cart)
+    end
+  end
+
+  def delete_cart(cart)
+    cart.destroy
   end
 end
